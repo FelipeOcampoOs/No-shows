e04169be07ef4dc80ede09cf789dcbcdb934f71e
Felipe Ocampo Osorio <119126865+FelipeOcampoOs@users.noreply.github.com>
2025-05-04 12:33:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from io import BytesIO
 
 st.title("🩺 Predicción de Asistencia a Citas Médicas")
 
-uploaded_file = st.file_uploader("Sube tu archivo .xlsx", type=["xlsx", "XLSX"])
+uploaded_file = st.file_uploader("Sube tu archivo .xlsx")
+
 
 
 if uploaded_file is not None:
